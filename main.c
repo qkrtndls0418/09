64d8239e844142f67b3c84119262efbfc8dd3e90
qkrtndls0418 <si616979@gmail.com>
2025-10-30 16:01:45 +0900

    09

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5 //전처리기 SIZE를 5로 바꿔줌
+#define SIZE 5 
  
 
 int main(int argc, char *argv[])
 {
-    int i, average;
-    int sum;
+    int i;
     int grade[SIZE];
+    int score[SIZE];
     
-    sum = 0;
-    for(i=0;i<SIZE;i++)
-    {
-                        printf("학생 성적을 입력하세요:");
-                        scanf("%d", &grade[i]);
-                        sum += grade[i];
-                        }
-     average = sum/SIZE;
-     printf("성적 평균:%d\n", average); 
+    for (i=0; i<SIZE; i++)
+    {    grade[i] = rand() % 100;
+    }
+    
+    for (i=0;i<SIZE;i++)
+    { score[i] = grade[i];
+}
+        
+        
+    for(i=0; i<SIZE; i++)
+             printf("grade[%i] = %i, score[%i] = %i\n",i, grade[i], i,score[i]);
+    
   
   system("PAUSE");	
   return 0;
